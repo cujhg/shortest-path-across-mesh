fc71a4bbd59277afe2bd46255e7e08a4883d7eca
ge72wan <ge72wan@mytum.de>
2021-01-20 20:17:34 +0100

    update

---
+++ CMakeFiles/cgal_shortest_path.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cgal_shortest_path.dir/cgal_shortest_path.cpp.o"
+  "cgal_shortest_path.pdb"
+  "cgal_shortest_path.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cgal_shortest_path.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- example.py
+++ example.py
@@ -13,7 +13,7 @@ mesh_from_data = cgal_shortest_path.cgal_shortest_path('data/elephant.off')
 point_1 = mesh_from_data.intersect([1.0, 1.0, 1.0], [-1.0, -1.0, -1.0])
 point_2 = mesh_from_data.intersect([1.0, 0.5, 1.0], [-1.0, -0.5, -1.0])
 
-path = mesh_from_data.shortest_path([0.1, -0.3, 0.25], [0, 1, 0])
+path = mesh_from_data.shortest_path([-0.5, -0.5, -0.25], [0, 1, 0])
 distance = mesh_from_data.shortest_distance([1, 0, 0], [0, 1, 0])
 
 print(distance)
